ec9ef1ecf2c639c1ea1baf0d9a00a300f94fee19
GregT <taylor.gregory2@gmail.com>
2022-06-07 22:49:11 -0600

    commit tree 15234e41076c8c26f21e0cd9c16a0bbbf62bfbac

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown int
+
+    while int > 0 do
+       puts "#{int} SECOND(S)!"
+    int = int - 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep int
+        while int > 0 do
+            puts "#{int} SECOND(S)!"
+        int = int - 1
+        sleep(1)
+        end
+        "HAPPY NEW YEAR!"
+    end
